f1c81be6420dbeb2aed86e2608615d7b4effa195
lunaticurey <125438787@qq.com>
2021-01-13 21:34:42 +0800

    add fucs

--- diy.sh
+++ diy.sh
@@ -10,8 +10,14 @@
 sleep 3
 rm -rf package/lean/luci-app-ssr-plus package/lean/default-settings
 sleep 3
+
         for i in $( ls mypatch ); do
             echo Applying mypatch $i
             patch -p1 < mypatch/$i
         done
-  
+sleep 6
+for file in *.config
+do
+echo "CONFIG_PACKAGE_luci-app-control-weburl=y" >> $file
+echo "CONFIG_PACKAGE_luci-app-fileassistant=y" >> $file
+done
